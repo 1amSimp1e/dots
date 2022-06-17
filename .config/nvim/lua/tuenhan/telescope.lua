58b94b851dfa4a29d2cb97fd054810804319bd68
iamverysimp1e <ngyntuenhan@protonmail.com>
2022-06-17 11:56:09 +0700

    Telescope Plugins Configurations

---
+++ .config/nvim/lua/tuenhan/telescope.lua
@@ -0,0 +1,16 @@
+function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
+
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+telescope.setup{
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+  }
+}
