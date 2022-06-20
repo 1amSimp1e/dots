b3fab99d90877cde08b8e7a7390ec9b2cf33a4e7
iamverysimp1e <ngyntuenhan@protonmail.com>
2022-06-20 21:51:43 +0700

    Added Everforest themes

--- .config/nvim/lua/tuenhan/lualine.lua
+++ .config/nvim/lua/tuenhan/lualine.lua
@@ -5,6 +5,7 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'solarized_dark',
+    --theme = 'everforest',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {}
