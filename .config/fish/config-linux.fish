b0bcdbf251d359bc222f2d1f52f20972038f3cda
iamverysimp1e <ngyntuenhan@protonmail.com>
2022-07-03 19:55:14 +0700

    Added ls icons options

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,7 +1,8 @@
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
-  alias llt "ll --tree --level=2 -a"
+  alias llta "ll --tree --level=2 -a"
+  alias llt "ll --tree --level=2"
   alias p "python3"
   alias nv "nvim"
   alias mutter-settings "cd ~/Downloads/mutter-rounded-setting && gjs dist/mutter_settings.js"
