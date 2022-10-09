c7829c8918c2edd29af0f34c49bfa7ff2244fa0f
Nguyen Tue Nhan <ngyn.tuenhan@protonmail.com>
2022-10-08 21:01:06 -0500

    Added my new custom shell prompt

--- .config/windows/powershell/user_profile.ps1
+++ .config/windows/powershell/user_profile.ps1
@@ -1,34 +1,18 @@
-# set PowerShell to UTF-8
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
-Import-Module -Name Terminal-Icons
-
-# PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias ll ls
-Set-Alias g git
-Set-Alias grep findstr
+#Alias 
+Set-Alias vim nvim 
+Set-Alias ll ls 
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
+# Prompt 
+$omp_config = Join-Path $PSScriptRoot ".\awsome_prompt.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+# Completions 
+
+
+# Ultilities 
+
+  # Terminal Icons 
+Import-Module -Name Terminal-Icons
+
