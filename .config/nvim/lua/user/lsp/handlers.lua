4a8905645b7ec574c1515b3e83c873130931dd4e
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-11-27 07:41:43 -0600

    Lsp -- Fixed icons diagnostics

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -12,10 +12,10 @@ M.capabilities =  cmp_nvim_lsp.default_capabilities(M.capabilities)
 M.setup = function()
 	local signs = {
 
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
+		{ name = "DiagnosticSignError", text = " " },
+		{ name = "DiagnosticSignWarn", text = " " },
+		{ name = "DiagnosticSignHint", text = " " },
+		{ name = "DiagnosticSignInfo", text = " " },
 	}
 
 	for _, sign in ipairs(signs) do
