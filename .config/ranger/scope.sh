cf08a0caf04d15118f306e8233b5ef9c789afcb2
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-11-29 20:21:23 -0600

    Ranger Configs -- Updated preview images using kitty

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -65,14 +65,14 @@ handle_extension() {
             exit 1;;
 
         ## PDF
-        pdf)
-            ## Preview as text conversion
-            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
-              fmt -w "${PV_WIDTH}" && exit 5
-            mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
-              fmt -w "${PV_WIDTH}" && exit 5
-            exiftool "${FILE_PATH}" && exit 5
-            exit 1;;
+        # pdf)
+        #     ## Preview as text conversion
+        #     pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
+        #       fmt -w "${PV_WIDTH}" && exit 5
+        #     mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
+        #       fmt -w "${PV_WIDTH}" && exit 5
+        #     exiftool "${FILE_PATH}" && exit 5
+        #     exit 1;;
 
         ## BitTorrent
         torrent)
