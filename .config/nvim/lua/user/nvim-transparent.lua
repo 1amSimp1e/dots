7bc46023adc74b3299d92afba0f64517dcb6143f
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-12-10 22:02:35 -0600

    Bufferline configs -- Updated

--- .config/nvim/lua/user/nvim-transparent.lua
+++ .config/nvim/lua/user/nvim-transparent.lua
@@ -2,14 +2,9 @@ require("transparent").setup({
   enable = true, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
-
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
+    'all'
   },
-  exclude = {}, -- table: groups you don't want to clear
+  exclude = {
+
+  }, -- table: groups you don't want to clear
 })
