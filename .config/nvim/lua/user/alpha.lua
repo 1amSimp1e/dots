43638fe6c332c86efdca56f48808d1cdc6149b07
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-12-17 15:02:02 -0600

    Alpha Configs -- Updated

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -34,6 +34,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
