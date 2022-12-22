d7ee0fee9ac8928c1ead988cbb1b9e6777bf2e9a
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-12-22 13:42:19 -0600

    Noice Configs -- Updated some stuff

--- .config/nvim/lua/s1mple/noice.lua
+++ .config/nvim/lua/s1mple/noice.lua
@@ -2,9 +2,9 @@ require("noice").setup({
 	lsp = {
 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
+			["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+			["vim.lsp.util.stylize_markdown"] = false,
+			["cmp.entry.get_documentation"] = false,
 		},
 	},
 	-- you can enable a preset for easier configuration
