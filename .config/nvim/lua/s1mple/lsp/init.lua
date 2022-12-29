8c7ab304ff1457b26c1bbbfc960880346f8de5e2
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2022-12-28 19:35:10 -0600

    Removed & start from scratch

--- .config/nvim/lua/s1mple/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require("s1mple.lsp.lspsaga")
-require("s1mple.lsp.configs")
-require("s1mple.lsp.handlers").setup()
-require("s1mple.lsp.null-ls")
--- .config/nvim/lua/s1mple/syntax.lua
+++
@@ -1,6 +0,0 @@
-require("poimandres").setup({
-  comment_style = "NONE",
-  keyword_style = "NONE",
-  function_style = "NONE",
-  variable_style = "NONE",
-})
