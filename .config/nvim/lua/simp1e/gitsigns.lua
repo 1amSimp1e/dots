3a2744c80161acef4b5762b7dcd48234ac727611
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-01 20:29:53 -0600

    Gitsigns -- Updated

--- .config/nvim/lua/simp1e/gitsigns.lua
+++ .config/nvim/lua/simp1e/gitsigns.lua
@@ -12,11 +12,13 @@ gitsigns.setup {
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+
   watch_gitdir = {
     interval = 1000,
     follow_files = true,
   },
   attach_to_untracked = true,
+  current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
