ab43b0e0e1fe3d83505ffcf385950c663679dfed
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-01 20:28:04 -0600

    Cmp configs -- Added sorting

--- .config/nvim/lua/simp1e/cmp.lua
+++ .config/nvim/lua/simp1e/cmp.lua
@@ -123,6 +123,18 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			require("cmp-under-comparator").under,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 	experimental = {
 		ghost_text = true,
 	},
