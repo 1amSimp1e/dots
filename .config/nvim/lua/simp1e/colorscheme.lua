2410ab6b0b515b810ee2019e76898a1987e3f28d
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-01 20:29:45 -0600

    Nvim Colorscheme --  added

--- .config/nvim/lua/simp1e/colorscheme.lua
+++ .config/nvim/lua/simp1e/colorscheme.lua
@@ -2,26 +2,26 @@ local colorscheme = "tokyonight-night"
 
 -- Transparent
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    local hl_groups = {
-      "Normal",
-      "SignColumn",
-      "NormalNC",
-      "TelescopeBorder",
-      "NvimTreeNormal",
-      "EndOfBuffer",
-      "MsgArea",
-    }
-    for _, name in ipairs(hl_groups) do
-      vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
-    end
-  end,
+	pattern = "*",
+	callback = function()
+		local hl_groups = {
+			"Normal",
+			"SignColumn",
+			"NormalNC",
+			"TelescopeBorder",
+			"NvimTreeNormal",
+			"EndOfBuffer",
+			"MsgArea",
+		}
+		for _, name in ipairs(hl_groups) do
+			vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
+		end
+	end,
 })
 
 vim.opt.fillchars = "eob: "
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
