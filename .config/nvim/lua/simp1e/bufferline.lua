dd3255245ae78fcc797244cca19a9d973ee7d0b7
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-01-01 20:27:57 -0600

    Bufferline configs -- Update some stuff

--- .config/nvim/lua/simp1e/bufferline.lua
+++ .config/nvim/lua/simp1e/bufferline.lua
@@ -9,6 +9,7 @@ bufferline.setup {
     right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+    color_icons = true,
   },
 
   highlights = {
