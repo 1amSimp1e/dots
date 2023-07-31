5d4952b8c4c357fe78cca44b12480313d862fd1d
1amSimp1e <ngyn.tuenhan@protonmail.com>
2023-07-31 14:23:05 -0500

    ✅ Removed unecessary stuffs + system menu

--- configs/eww/scripts/battery.sh
+++ configs/eww/scripts/battery.sh
@@ -21,10 +21,10 @@ elif [ "$per" -gt "20" ]; then
 	icon="assets/battery_nearly-low.png"
 elif [ "$per" -gt "10" ]; then
 	icon="assets/battery_low.png"
-	# notify-send -u critical "Battery Low" "Connect Charger"
+	notify-send -u critical "Battery Low" "Connect Charger"
 elif [ "$per" -gt "0" ]; then
 	icon="assets/battery_low.png"
-	# notify-send -u critical "Battery Low" "Connect Charger"
+	notify-send -u critical "Battery Low" "Connect Charger"
 else
         echo  && exit
 fi
@@ -42,7 +42,34 @@ status(){
     echo ""
 }
 
+class() {
+    if [ "$per" -gt "90" ]; then
+        class="color: #81c19b"
+    elif [ "$per" -gt "80" ]; then
+        class="color: #81c19b"
+    elif [ "$per" -gt "70" ]; then
+        class="color: #81c19b"
+    elif [ "$per" -gt "60" ]; then
+        class="color: #81c19b"
+    elif [ "$per" -gt "49" ]; then
+        class="color: #8FA7A9"
+    elif [ "$per" -gt "40" ]; then
+        class="color: #8FA7A9"
+    elif [ "$per" -gt "20" ]; then
+        class="color: #ecd28b"
+    elif [ "$per" -gt "10" ]; then
+        class="color: #df5b61"
+    elif [ "$per" -gt "0" ]; then
+        class="color: #df5b61"
+    else
+            echo "" && exit
+    fi
+    echo "$class"
+}
+
 [ "$1" = "icon" ] && icon && exit
 [ "$1" = "percent" ] && percent && exit
 [ "$1" = "status" ] && status && exit
+[ "$1" = "class" ] && class && exit
+
 exit
